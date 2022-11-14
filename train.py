d0538e44bdfb3da9505e528527cbaaee5583e8ca
b08902023 <b08902023@meow2.csie.ntu.edu.tw>
2022-11-14 22:46:35 +0800

    update tqdm desc

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import dataset
 import render
 import model
 import argparse
-import matplotlib.pyplot as plt
 from PIL import Image
 
 parser = argparse.ArgumentParser()
@@ -40,12 +39,21 @@ else:
     device = torch.device('cpu')
 print(f'using {device} as device')
 
-def train(train_dataset):
-    encoder = model.IntegratedPositionalEncoder(freq_range=[args.freq_low, args.freq_high]).to(device)
-    dir_encoder = model.PositionalEncoder(input_size=2, freq_range=[0, 4]).to(device)
-    
-    nerf_model = model.NerfModel(input_dim=encoder.output_size(), 
-                                input_dim_dir=dir_encoder.output_size()).to(device)
+def forward(o, d, r, t_vals, nerf_model, encoder, dir_encoder):
+    encoded = encoder(o, d, r, t_vals[:, :-1, :], t_vals[:, 1:, :])
+    dirs = d / torch.norm(d, dim=-1, keepdim=True)
+    sphericals = render.get_spherical(dirs)
+    dirs_encoded = dir_encoder(sphericals)
+    density, rgb = nerf_model(encoded, dirs_encoded)
+    density = torch.unsqueeze(density, 2)
+    delta_t = t_vals[:, 1:, :] - t_vals[:, :-1, :]
+    delta_t *= torch.norm(d, dim=-1, keepdim=True)
+    weights = render.integrate_weights(density, delta_t)
+    rgba = torch.cat([rgb, torch.ones(*weights.shape).to(device)], dim=2)
+    result = torch.sum(rgba * weights, dim=1)
+    return weights, result
+
+def train(train_dataset, nerf_model, encoder, dir_encoder):
     opt = torch.optim.Adam(nerf_model.parameters(), lr=args.lr)
 
     def lr_lambda(i):
@@ -72,25 +80,11 @@ def train(train_dataset):
                             batch_size=args.batch_size,
                             shuffle=True)
 
-    def forward(o, d, r, t_vals):
-        encoded = encoder(o, d, r, t_vals[:, :-1, :], t_vals[:, 1:, :])
-        dirs = d / torch.norm(d, dim=-1, keepdim=True)
-        sphericals = render.get_spherical(dirs)
-        dirs_encoded = dir_encoder(sphericals)
-        density, rgb = nerf_model(encoded, dirs_encoded)
-        density = torch.unsqueeze(density, 2)
-        delta_t = t_vals[:, 1:, :] - t_vals[:, :-1, :]
-        delta_t *= torch.norm(d, dim=-1, keepdim=True)
-        weights = render.integrate_weights(density, delta_t)
-        rgba = torch.cat([rgb, torch.ones(args.batch_size, t_vals.shape[1]-1, 1).to(device)], dim=2)
-        result = torch.sum(rgba * weights, dim=1)
-        return weights, result
-
     nerf_model.train()
     step_counter = 0
     count = 0
     for epoch in range(args.epoch):
-        for o, d, ground_truth in tqdm.tqdm(dataloader):
+        for it, (o, d, ground_truth) in enumerate(pbar:= tqdm.tqdm(dataloader)):
             step_counter += 1
             o = o.to(device)
             d = d.to(device)
@@ -98,48 +92,44 @@ def train(train_dataset):
             r = 1 / train_dataset.focal / 1.732 # sqrt(3)
             o = o.reshape([args.batch_size, 1, 3])
             d = d.reshape([args.batch_size, 1, 3])
-            
+
             t_vals = render.gen_intervals(args.near, args.far, args.batch_size, args.sample_per_ray)
             t_vals = t_vals.to(device)
 
-            weights_coarse, result_coarse = forward(o, d, r, t_vals)
+            weights_coarse, result_coarse = forward(o, d, r, t_vals, nerf_model, encoder, dir_encoder)
 
             fine_sampler = render.FineSampler2(weights_coarse, t_vals, alpha=0.001)
-            fine_t_vals = fine_sampler.sample(args.fine_sample_per_ray + 1, random=False).detach()
+            fine_t_vals = fine_sampler.sample(args.fine_sample_per_ray + 1, random=True).detach()
 
-            weights_fine, result_fine = forward(o, d, r, fine_t_vals)
+            weights_fine, result_fine = forward(o, d, r, fine_t_vals, nerf_model, encoder, dir_encoder)
 
             coarse_loss = loss_func(result_coarse, ground_truth)
             fine_loss = loss_func(result_fine, ground_truth)
             loss = fine_loss + args.lambda_loss * coarse_loss
+
             opt.zero_grad()
             loss.backward()
             opt.step()
             sch.step()
-            count += 1
-            if count == 100:
-                count = 0
-                print(f'lr: {opt.param_groups[0]["lr"]}')
-                print(f'coarse_loss: {coarse_loss}')
-                print(f'fine_loss: {fine_loss}')
-                print(f'loss: {loss}')
-                print(f'max_alpha: {torch.max(result_fine[..., -1])}')
-                print(f'min_alpha: {torch.min(result_fine[..., -1])}')
+
+            summary = [
+                f'epoch {epoch}',
+                f'iteration {it}/{len(dataloader)}',
+                f'loss: {coarse_loss:.3e}/{fine_loss:.3e}/{loss:.3e}',
+                f'alpha: {torch.max(result_fine[..., -1:]):.2f}/{torch.min(result_fine[..., -1:]):.2f}'
+            ]
+            pbar.set_description(' | '.join(summary))
         torch.save({
             'model': nerf_model.state_dict(),
             'opt': opt.state_dict(),
             'sch': sch.state_dict()
             }, args.ckpt)
 
-def test(test_dataset):
+def test(test_dataset, nerf_model, encoder, dir_encoder):
     dataloader = DataLoader(test_dataset, 
                             batch_size=test_dataset.width,
                             shuffle=False)
-    encoder = model.IntegratedPositionalEncoder(freq_range=[args.freq_low, args.freq_high]).to(device)
-    dir_encoder = model.PositionalEncoder(input_size=2, freq_range=[0, 4]).to(device)
-    nerf_model = model.NerfModel(input_dim=encoder.output_size(), 
-                                input_dim_dir=dir_encoder.output_size()).to(device)
-    nerf_model.load_state_dict(torch.load(args.ckpt)['model'])
+    nerf_model.load_state_dict(torch.load(args.ckpt['model']))
     nerf_model.eval()
     with torch.no_grad():
         rows = []
@@ -148,34 +138,20 @@ def test(test_dataset):
             o = o.to(device)
             d = d.to(device)
             ground_truth = ground_truth.to(device)
-            t_vals = render.gen_intervals(args.near, args.far, test_dataset.width, args.sample_per_ray)
-            t_vals = t_vals.to(device)
             r = 1 / test_dataset.focal / 1.732 # sqrt(3)
             o = o.reshape([test_dataset.width, 1, 3])
             d = d.reshape([test_dataset.width, 1, 3])
 
-            encoded = encoder(o, d, r, t_vals[:, :-1, :], t_vals[:, 1:, :])
-            dirs = d / torch.norm(d, dim=-1, keepdim=True)
-            sphericals = render.get_spherical(dirs)
-            dirs_encoded = dir_encoder(sphericals)
-            density, rgb = nerf_model(encoded, dirs_encoded)
-            density = torch.unsqueeze(density, 2)
-            delta_t = t_vals[:, 1:, :] - t_vals[:, :-1, :]
-            delta_t *= torch.norm(d, dim=-1, keepdim=True)
-            weights = render.integrate_weights(density, delta_t)
+            t_vals = render.gen_intervals(args.near, args.far, test_dataset.width, args.sample_per_ray)
+            t_vals = t_vals.to(device)
 
-            fine_sampler = render.FineSampler(weights, t_vals, alpha=0.001)
-            fine_t_vals = fine_sampler.sample(args.fine_sample_per_ray + 1)
+            weights_coarse, _ = forward(o, d, r, t_vals, nerf_model, encoder, dir_encoder)
 
-            encoded_fine = encoder(o, d, r, fine_t_vals[:, :-1, :], fine_t_vals[:, 1:, :])
-            density_fine, rgb_fine = nerf_model(encoded_fine, dirs_encoded)
-            density_fine = torch.unsqueeze(density_fine, 2)
-            delta_t_fine = fine_t_vals[:, 1:, :] - fine_t_vals[:, :-1, :]
-            delta_t_fine *= torch.norm(d, dim=-1, keepdim=True)
-            weights_fine = render.integrate_weights(density_fine, delta_t_fine)
-            rgba_fine = torch.cat([rgb_fine, torch.ones(test_dataset.width, args.fine_sample_per_ray, 1).to(device)], dim=2)
-            result_fine = rgba_fine * weights_fine
-            result_fine = torch.sum(result_fine, dim=1)
+            fine_sampler = render.FineSampler2(weights_coarse, t_vals, alpha=0.001)
+            fine_t_vals = fine_sampler.sample(args.fine_sample_per_ray + 1, random=True).detach()
+
+            _, result_fine = forward(o, d, r, fine_t_vals, nerf_model, encoder, dir_encoder)
+
             rows.append(result_fine)
             if len(rows) % test_dataset.height == 0:
                 img = torch.stack(rows)
@@ -189,15 +165,15 @@ def test(test_dataset):
 
 
 if __name__ == "__main__":
+    encoder = model.IntegratedPositionalEncoder(freq_range=[args.freq_low, args.freq_high]).to(device)
+    dir_encoder = model.PositionalEncoder(input_size=2, freq_range=[0, 4]).to(device)
+    nerf_model = model.NerfModel(input_dim=encoder.output_size(), 
+                                input_dim_dir=dir_encoder.output_size()).to(device)
     raw_data = dataset.load_synthetic(args.data_path, verbose=True)
     
     if args.mode == 'train':
         train_dataset = dataset.PixelDataset(raw_data['train'], verbose=True)
-        train(train_dataset)
+        train(train_dataset, nerf_model, encoder, dir_encoder)
     elif args.mode == 'test':
         test_dataset = dataset.PixelDataset(raw_data['test'], verbose=True)
-        test(test_dataset)
-    elif args.mode == 'debug':
-        t = torch.linspace(-4, 4, 1025)
-        t0 = t[:-1].reshape([1, -1, 1])
-        t1 = t[1:].reshape([1, -1, 1])
+        test(test_dataset, nerf_model, encoder, dir_encoder)
